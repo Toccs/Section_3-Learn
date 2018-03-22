28c50de17ae802c5c3949765b9e40d246fb69a34
Toccs <37660664+Toccs@users.noreply.github.com>
2018-03-22 12:45:41 +0000

    Initial commit

---
+++ Source/Section_3/Section_3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Section_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Section_3, "Section_3" );
---
+++ Source/Section_3/Section_3GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Section_3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_3_API ASection_3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
